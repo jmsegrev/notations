f83aba073402d4a40cf8c4fe53c673b07e5a240a
juan <juanmanuel@verges.com.do>
2014-11-07 18:27:22 -0400

    Update test with example

--- notations_test.go
+++ notations_test.go
@@ -1,6 +1,11 @@
 package notations
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/jmsegrev/notations"
+)
 
 func TestExtract(t *testing.T) {
 
@@ -32,5 +37,30 @@ func TestExtract(t *testing.T) {
 	if n.Value != "1223.93" && text[n.Slice.Begin:n.Slice.End] != n.Value {
 		t.Error("Failed to extract amount")
 	}
+
+}
+
+func ExampleExtract() {
+
+	txt := `@username assume this is a reply, by @anotherusername that has
+	some #tag1 and #tag2. It also works the same way for %any &other *symbol,
+	but you provably will only use things like $2377.12.`
+
+	notationsMap := notations.Extract(txt, "@#$%&*$")
+	for key, notations := range notationsMap {
+		fmt.Printf("%s: ", key)
+		for _, notation := range notations {
+			fmt.Printf("%s ", notation.Value)
+		}
+		fmt.Println("")
+	}
+
+	// Output:
+	// @: username anotherusername
+	// #: tag1 tag2
+	// %: any
+	// &: other
+	// *: symbol
+	// $: 2377.12
 
 }
